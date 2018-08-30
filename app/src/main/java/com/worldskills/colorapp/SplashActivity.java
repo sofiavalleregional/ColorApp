d85fa9f7fad864f38b6622da5645480887658436
alexander casanova <casanova8526@gmail.com>
2018-08-30 11:48:56 -0500

    alex

--- app/src/main/java/com/worldskills/colorapp/SplashActivity.java
+++ app/src/main/java/com/worldskills/colorapp/SplashActivity.java
@@ -1,7 +1,11 @@
 package com.worldskills.colorapp;
 
+import android.content.Intent;
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import com.worldskills.colorapp.activities.Home;
 
 public class SplashActivity extends AppCompatActivity {
 
@@ -9,6 +13,20 @@ public class SplashActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                iniciarHome();
+            }
+        },2000);
+    }
+
+    /*Metodo que se encarga de iniciar la actividad home*/
+    public void iniciarHome(){
+        Intent intent=new Intent(this,Home.class);
+        startActivity(intent);
+        finish();
     }
 }
 
